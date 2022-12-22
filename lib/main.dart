64dab45986dbd3c1846e285d0d687ec7dd74798c
d.florentina.calapod <>
2022-12-22 14:42:04 +0100

    41-how-to-use-container-widgets+single-child

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, //hide debug banner
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            // MARGIN
+            // margin: EdgeInsets.all(10.0),
+            // margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+            // margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
+            margin: EdgeInsets.only(left: 30.0),
+            // PADDING
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
